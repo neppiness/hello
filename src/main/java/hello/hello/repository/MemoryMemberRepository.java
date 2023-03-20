f36bcfdaf7440f9ffebf557efed6fbfb298d1719
neppiness <0414kjh@naver.com>
2023-03-20 17:20:01 +0900

    update: member mangement logic implementation study

---
+++ src/main/java/hello/hello/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hello.repository;
+
+import hello.hello.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    // 루프를 돌면서, 같은 값을 하나라도 찾게 되면 반환하는 로직.
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
