ccc4fd4be16f9aacb10e74e9e7c9fc7d965ecc08
neppiness <0414kjh@naver.com>
2023-04-06 13:14:00 +0900

    update: execution time checking logic added

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,7 +1,6 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -9,34 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-<<<<<<< HEAD
     //    private DataSource dataSource;
     private final MemberRepository memberRepository;
 
     @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-=======
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
->>>>>>> 6336f3dcdc1dfe1c1f59087eb8dfaf473dc9e08d
     }
 
 //    @Autowired
@@ -53,12 +35,6 @@ public class SpringConfig {
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcTemplateMemberRepository(dataSource);
-<<<<<<< HEAD
 //    }
 }
 
-=======
-        return new JpaMemberRepository(em);
-    }
-}
->>>>>>> 6336f3dcdc1dfe1c1f59087eb8dfaf473dc9e08d
